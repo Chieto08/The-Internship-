61a7d04ebdda2197aaf0454e545ad7f938f9944f
LAPTOP-699E6IQM\CHIETO <cuee26@gmail.com>
2019-01-18 08:53:15 +0100

    Modified Greet User.py and Number Guess.py

--- Greet user.py
+++ Greet user.py
@@ -1,5 +1,3 @@
 username = input("Enter your name: ")
 if username == "Alice" or username == "Bob":
-    print("Hello " + username)
-else:
-    pass
+    print("Good day " + username)
--- Number Guess.py
+++ Number Guess.py
@@ -4,8 +4,8 @@ random_number = random.randint(1, 100)
 number_of_guesses = 0
 guessed_number = 0
 while guessed_number != random_number:
-    number_of_guesses = number_of_guesses + 1
     guessed_number = int(input("Make a guess from 1-100: "))
+    number_of_guesses = number_of_guesses + 1
     if guessed_number < random_number:
         print("Number is lower than correct number, guess again.")
     elif guessed_number > random_number:
