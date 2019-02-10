831e396f3f9b66103643bbaa29342f4db2ba3c58
LAPTOP-699E6IQM\CHIETO <cuee26@gmail.com>
2019-02-10 20:28:38 +0100

    readme file added for the Dice Game project.

--- Week 3/Dice Game.py
+++ Week 3/Dice Game.py
@@ -3,82 +3,82 @@ import random
 def start_game():
     print(" Welcome to 'THE DICE GAME'. This game involves rolling 2 dices in \n which the player which has the highest score when both scores of the dices rolled \n is calculated, emerges as winner. At least 2 players are required to be active on \n this game, and a maximum of 100 players. Best of luck")
     no_of_players = int(input("How many players are active? "))
-    user_details = []
+    player_playernames = []
     for name in range(no_of_players):
         if no_of_players>= 2 and no_of_players<= 100:
-            user_names = input("Enter the name of user " + str(name + 1) + ":" )
-            while user_names in user_details:
-                user_names = input("Enter the name of user " + str(name + 1) + ",name " + user_names + " is taken:" )
+            player_names = input("Enter the name of player " + str(name + 1) + ":" )
+            while player_names in player_playernames:
+                player_names = input("Enter the name of player " + str(name + 1) + ",name " + player_names + " is taken:" )
             else:
-                user_details.append(user_names)
+                player_playernames.append(player_names)
         else:
-            print("Invalid number of users.")
+            print("Invalid number of players.")
             exit()
-    return user_details
+    return player_playernames
 
-def roll_dice(user_details):
-    user_details_and_scores = {}
+def roll_dice(player_playernames):
+    player_playernames_and_scores = {}
     for trial in range(3):
-        for user in user_details:
-            roll = input("Hey " + user + " enter 1 to roll a dice: ") 
+        for player in player_playernames:
+            roll = input("Hey " + player + " enter 1 to roll a dice: ") 
             while roll != "1":
-                roll = input("Hey " + user + ", " + roll + " does not equal 1. Enter 1 to roll a dice: ")      
+                roll = input("Hey " + player + ", " + roll + " does not equal 1. Enter 1 to roll a dice: ")      
             else:
                 dice1 = random.randint(1, 6)
                 dice2 = random.randint(1, 6)
-                print("Results for " + user+ " from trial " + str(trial + 1)+ ": Dice 1: " + str(dice1) + ", Dice 2: " + str(dice2))
+                print("Results for " + player+ " from trial " + str(trial + 1)+ ": Dice 1: " + str(dice1) + ", Dice 2: " + str(dice2))
                 total_score = dice1 + dice2
-                if (is_a_valid_key(user_details_and_scores,user)) == False:
-                    user_details_and_scores[user] = []
-                    user_details_and_scores[user].append(total_score)
-                elif (is_a_valid_key(user_details_and_scores,user)) == True:
-                    user_details_and_scores[user].append(total_score)
+                if (is_a_valid_key(player_playernames_and_scores,player)) == False:
+                    player_playernames_and_scores[player] = []
+                    player_playernames_and_scores[player].append(total_score)
+                elif (is_a_valid_key(player_playernames_and_scores,player)) == True:
+                    player_playernames_and_scores[player].append(total_score)
                 
-    return user_details_and_scores
+    return player_playernames_and_scores
 
-def get_highest_score(user_details_and_scores):
-    user_details_and_highest_scores = {}
-    for user in user_details_and_scores:
-        user_details_and_highest_scores[user] = max(user_details_and_scores[user])
-    return user_details_and_highest_scores
+def get_highest_score(player_playernames_and_scores):
+    player_playernames_and_highest_scores = {}
+    for player in player_playernames_and_scores:
+        player_playernames_and_highest_scores[player] = max(player_playernames_and_scores[player])
+    return player_playernames_and_highest_scores
 
-def choose_winner(user_details):
-    user_details_and_scores = roll_dice(user_details)
-    user_details_and_highest_scores = get_highest_score(user_details_and_scores)
+def choose_winner(player_playernames):
+    player_playernames_and_scores = roll_dice(player_playernames)
+    player_playernames_and_highest_scores = get_highest_score(player_playernames_and_scores)
     maximum_element = 0
     final_winner = ""
-    users_max = []
-    for value in user_details_and_highest_scores.values():
-        users_max.append(value)
-    maximum_element = max(users_max)
-    if check_tie(users_max, maximum_element) == True:
+    players_max = []
+    for value in player_playernames_and_highest_scores.values():
+        players_max.append(value)
+    maximum_element = max(players_max)
+    if check_tie(players_max, maximum_element) == True:
         print("Tied")
-        tied_users = get_key(user_details_and_highest_scores, maximum_element)
-        users_max = []
-        resolve_tie(tied_users)
-    elif check_tie(users_max, maximum_element) == False:
-        the_winner = get_key(user_details_and_highest_scores, maximum_element)
+        tied_players = get_key(player_playernames_and_highest_scores, maximum_element)
+        players_max = []
+        resolve_tie(tied_players)
+    elif check_tie(players_max, maximum_element) == False:
+        the_winner = get_key(player_playernames_and_highest_scores, maximum_element)
         for winner in the_winner:
             final_winner = winner
         return "The winner is " + final_winner + " with a score of " + str(maximum_element)
 
-def check_tie(users_max, maximum_element):
-    if users_max.count(maximum_element) > 1:
+def check_tie(players_max, maximum_element):
+    if players_max.count(maximum_element) > 1:
         return True
-    elif users_max.count(maximum_element) == 1:
+    elif players_max.count(maximum_element) == 1:
         return False
  
-def resolve_tie(tied_users):
-    print("Tie between " + str(tied_users))
-    print(choose_winner(tied_users))
+def resolve_tie(tied_players):
+    print("Tie between " + str(tied_players))
+    print(choose_winner(tied_players))
     
 
 def get_key(dictionary, val):
      key = [key for key,value in dictionary.items() if value == val]
      return key
 
-def is_a_valid_key(dictionary, user):
-    if user in dictionary:
+def is_a_valid_key(dictionary, player):
+    if player in dictionary:
         return True
     else:
         return False
---
+++ Week 3/readme.md
@@ -0,0 +1,67 @@
+# DICE GAME
+
+### About
+**The dice game is a game which entails 2 - 100 players actively playing at a time. It involves rolling 2 dices, in which the player with the highest total score wins the game after 3 trials. If there exists a tie, then there is a replay till a single winner emerges.**
+
+---
+### Below are the functions and their tasks:
+### *start_game()*
+This function starts the game by explaining the rules of the game to the player, and accepting player usernames.
+
+Requires: Nothing.
+
+Returns: usernames of all active players.
+
+### *roll_dice()*
+This function performs the task of playing the game. Which is rolling a dice 3 times and matching a players results with the player's usernames.
+
+Requires: usernames of all active players.
+
+Returns: All player usernames, paired with their scores after 3 trials.
+
+### *get_highest_score()*
+This function gets the highest score gotten by each player after 3 trials and stores the player's usernames and the corresponding highest score in a dictionary.
+
+Requires: All player usernames, paired with their scores after 3 trials.
+
+Returns: All player usernames, paired with their highest score.
+
+### *choose_winner()*
+This function chooses the winner of the game by picking the player with the highest score out of other players.
+
+Requires:The player usernames, All player usernames, paired with their highest score.
+
+Returns: The winner of the game, if there is no tie. 
+
+### *check_tie()*
+This function checks if a tie exists at the end of a game by checking if the highest score appears multiple times.
+
+Requires: The highest score of all players.
+
+Returns: True if the highest score appears multipe times, False otherwise.
+
+### *resolve_tie()*
+This function resolves the pending tie by replaying the game from scratch.
+
+Requires: The tied players.
+
+Returns: Nothing.
+
+### *get_key()*
+This function gets the player's usernames by the player's score.
+
+Requires: All player usernames, paired with their highest score and the player's highest score.
+
+Returns: The player's usernames
+
+
+### *is_a_valid_key()*
+Checks if the player has previously played, if the player has then the new score is appended to the previous ones else a new list is initialized.
+
+Requires: 	The usernamess of all players paired with their scores, and the usernames of the particular player.
+
+Returns: True if the player has played, else False otherwise.
+
+
+
+ 
